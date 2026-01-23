11a040a97264f7aa204f1686162feabffc1cbd1c
iamkr2308 <kunalrajesh23@gmail.com>
2026-01-23 10:20:05 +0530

    JAVA First Program - HelloWorld

---
+++ Java/Core Java/Part 1 - Intro/HelloWorld/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args) {  
+        System.out.println("Hello World!!!");
+    }
+}
+
+// 1. public - It means it is accessible from anywhere.
+// 2. class Hello - Name as per the File name created, As java is a Object Oriented Langauge so everything should be inside the class.
+// 3. static - It is used for memory efiiciency, runs without an object need to be created as it belongs to the class.
+// 4. void - Means this method returns nothing
+// 5. main - It is important for running the program.
+// 6 - String[] args - The prgm can take input from the command line.
+// 7. System - Builtin Java Class that helps connect with the computer system like keyboard, screen.
+// 8. out - Special object inside 'System', that represents the output.
+// 9. println - Method that prints.
+ 
