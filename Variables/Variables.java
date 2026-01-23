56eeb1cf0db45cd060dd637cace57a7cec1885ff
iamkr2308 <kunalrajesh23@gmail.com>
2026-01-23 12:29:11 +0530

    Java Variables

---
+++ Variables/Variables.java
@@ -0,0 +1,25 @@
+// Program 1
+// public class Variables {
+//     public static void main(String[] args) {
+
+//         String a = "Kunal Rajesh";
+//         System.out.println(a);
+//         int age = 20;
+//         System.out.println("Age: " + age); 
+//     }
+// }
+
+// 1. a, age = Are the Variables.
+
+//Program 2
+public class Variables {
+    public static void main(String[] args) {
+
+        String firstname = "James ";
+        String lastname = "Bond";
+        String fullname = firstname + lastname;
+        System.out.println(fullname);
+    }
+}
+
+// firstname, lastname fullname are the variables.
