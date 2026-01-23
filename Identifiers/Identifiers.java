d3fcadea8afaf5de2c361776e44d7326c675ad7d
iamkr2308 <kunalrajesh23@gmail.com>
2026-01-23 12:43:08 +0530

    Java Identifiers

---
+++ Identifiers/Identifiers.java
@@ -0,0 +1,12 @@
+public class Identifiers {
+    public static void main(String[] args) {
+
+        int minutesPerHour = 60;
+        System.out.println("Minutes in an hour: " + minutesPerHour);
+
+        float totalmarks = 90.0f;
+        System.out.println("Total Marks obtained : " + totalmarks); 
+    }
+}
+
+// Identifiers - They come under variables, but Identifiers are properly defined variables. For eg; minutesPerHour, totalmarks, studentname.
