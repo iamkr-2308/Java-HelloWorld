0aad6a0c61c6ecfe9215cceb31da71cbc82be167
iamkr2308 <kunalrajesh23@gmail.com>
2026-01-31 11:34:12 +0530

    DataTypes Java

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        int a = 23;
+        float b = 8.06f;
+        String c = "James Bond";
+        char d = 'A';
+        boolean e = true;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
+
+// Java DataTypes - Int, FLoat, Char, Boolean, String.
+// Byte - -128 to 127
+// Short = -32768 to 32768
+// Long = -9223............ to 9223..........
+    
